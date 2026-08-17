3423ad196cc514b4bf35f1c1171f841c94447b7e
Shiftii <shiftii629@gmail.com>
2026-08-17 11:42:32 -0500

    Split dashboard into route-based app shell

---
+++ app/organizations/page.tsx
@@ -0,0 +1,7 @@
+import { DashboardRoute } from "@/app/components/DashboardRoute";
+
+export const dynamic = "force-dynamic";
+
+export default function OrganizationsPage() {
+  return <DashboardRoute view="organizations" />;
+}
---
+++ app/settings/page.tsx
@@ -0,0 +1,7 @@
+import { DashboardRoute } from "@/app/components/DashboardRoute";
+
+export const dynamic = "force-dynamic";
+
+export default function SettingsPage() {
+  return <DashboardRoute view="settings" />;
+}
